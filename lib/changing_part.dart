ef18032a13dabf1e82ca432c0075a6ec8eb5aa28
Indrajit <indrajit30623@gmail.com>
2024-03-15 00:46:51 +0530

    creted methods for changing screen

--- lib/changing_part.dart
+++ lib/changing_part.dart
@@ -2,23 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:to_do_list/small_container.dart';
 import 'package:to_do_list/data/data_array.dart';
 
-class ChangingPart extends StatefulWidget{
-  const ChangingPart({super.key});
+class ChangingPart extends StatefulWidget {
+  const ChangingPart(this.onTapAdd, {Key? key}) : super(key: key);
+  final void Function() onTapAdd;
+
   @override
   State<StatefulWidget> createState() {
-   return _ChangingPart();
+    return _ChangingPartState();
   }
 }
 
-class _ChangingPart extends State<ChangingPart> {
+class _ChangingPartState extends State<ChangingPart> {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-       SmallContainer(Text('txt1'),),
-       SmallContainer(Text('txt2'),),
-       SmallContainer(Text('txt3'),),
-       ...dataArr.map((e) => SmallContainer(Text(e),),),
+        SmallContainer(Text('txt1')),
+        SmallContainer(Text('txt2')),
+        SmallContainer(Text('txt3')),
+        ...dataArr.map((e) => SmallContainer(Text(e))),
+         Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: widget.onTapAdd,
+                      child: const Text('Add'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Clear All'),
+                    ),
+                  ],
+                ),
+        // ElevatedButton(
+        //   onPressed: widget.onTapAdd,
+        //   child: Text('Add'),
+        // ),
       ],
     );
   }
--- lib/to_do_app.dart
+++ lib/to_do_app.dart
@@ -1,45 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list/addscreen.dart';
 import 'package:to_do_list/changing_part.dart';
-import 'package:to_do_list/data/data_array.dart';
 
-class ToDoApp extends StatelessWidget{
-  const ToDoApp({super.key});
+class ToDoApp extends StatefulWidget {
+  const ToDoApp({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    
-    return(
-      MaterialApp(
-        home: Scaffold(
-          body: Container(decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Colors.purple,
-              Color.fromARGB(255, 226, 155, 239),
-            ],
-            begin: Alignment.topLeft ,
-            end: Alignment.bottomRight ,
-            ),
-          ),
-          child:  Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                 const ChangingPart(),
-                 Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                   children: [
-                    
-                     ElevatedButton(onPressed: (){}, child: const Text('Add')),
-                     ElevatedButton(onPressed: (){}, child: const Text('clear All'),),
-                   ],
-                 ),
-              ],
-            ),
-          ),
-          //ElevatedButton(onPressed: (){}, child: Text('data'),),
-          ),
-        ),
-      )
-    );
+  State<StatefulWidget> createState() {
+    return _ToDoAppState();
   }
 }
 
+class _ToDoAppState extends State<ToDoApp> {
+
+
+ Widget? activesccreen ;
+
+  @override
+  void initState() {
+   activesccreen = ChangingPart(onTapAdd);
+    super.initState();
+  }
+
+ void onTapAdd(){
+  setState(() {
+    activesccreen = AddScreen() ;  //////////////////////
+  });
+ }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.purple,
+                Color.fromARGB(255, 226, 155, 239),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(child: activesccreen),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
